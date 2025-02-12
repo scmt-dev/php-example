40c54bf8974c990eb428c37a3b30cd116e0b8266
Mark AI <chanthavong_12@hotmail.com>
2025-02-12 11:30:52 +0700

    comment db config

--- db.php
+++ db.php
@@ -1,6 +1,9 @@
 <?php 
+// open mode debug
 error_reporting(1);
+//                ip server    |user fb |pwd   |database name 
 $db = new mysqli('localhost', 'root', 'root', 'pos');
+// check error connection
 if ($db->connect_errno) {
     echo "Failed to connect to MySQL: " . $db->connect_error;
     exit();
