f5faa1d65e91a6403c9ca37ed63aa1780951ada1
Mark AI <chanthavong_12@hotmail.com>
2025-03-05 11:24:27 +0700

    user can post

--- index.php
+++ index.php
@@ -1,28 +1,48 @@
+<?php 
+session_start();
+
+if(isset($_POST['submit'])) {
+    $content = $_POST['content'];
+    include_once 'db.php';
+    $SQL = "INSERT INTO posts (content, user_id) VALUES (?,?)";
+    $stmt = $db->prepare($SQL);
+    $stmt->bind_param('si', $content, $_SESSION['user_id']);
+    $stmt->execute();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-        <?php 
-            // set time zone
-            date_default_timezone_set('Asia/Bangkok');
-            echo date('d/m/Y');
-        ?>
+        Home
     </title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-        $date = date('D d F,Y H:i:s');
-        $IP = $_SERVER['REMOTE_ADDR'];
-        file_put_contents('log.txt',$IP.' '.$date.PHP_EOL, FILE_APPEND);
-        echo $date;
-        $colors = ['green',13 , true];
-        echo $colors[0];
-        echo $colors[1];
-        var_dump($colors);
-        var_dump($date);
-        print_r($colors);
-    ?>
+
+    <nav class="flex">
+        <h1>Humblack</h1>
+       <div>
+       <input type="text">
+       </div>
+        <ul class="flex">
+            <li><?php echo $_SESSION['fullname'] ?></li>
+            <li>
+                <a href="logout.php">Logout</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div>
+        <form action="" method="post">
+            <img width="30" src="https://avatars.githubusercontent.com/u/18229355?v=4" alt="Hero Image">
+            <textarea name="content" placeholder="What's on your mind?"></textarea>
+            <button name="submit">post</button>
+        </form>
+    </div>
+
 </body>
 </html>
