8ebb82284ee2e01975c7317524d156e60bc3daeb
Mark AI <chanthavong_12@hotmail.com>
2025-03-05 10:01:30 +0700

    improve login page

--- login.php
+++ login.php
@@ -1,7 +1,41 @@
 
 
 <?php 
+session_start();
 $title = 'Login';
+$message = '';
+// check if the form is submitted
+if (isset($_POST['submit'])) {
+    // get the form data
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    // connect to the database
+    include_once 'db.php';
+    $QUERY = "SELECT * FROM users WHERE email = ?";
+    $STMT = $db->prepare($QUERY);
+    $STMT->bind_param('s', $email);
+    $STMT->execute();
+    $RESULT = $STMT->get_result();
+    // check if the user exists
+    if ($RESULT->num_rows > 0) {
+        $USER = $RESULT->fetch_assoc();
+        // check if the password is correct
+        $isValid = password_verify($password, $USER['password']);
+        if ($isValid) {
+            // set the session variables
+            $_SESSION['user_id'] = $USER['id'];
+            $_SESSION['fullname'] = $USER['full_name'];
+            // redirect to the home page
+            header("Location: index.php");
+        }else {
+            $message = "Invalid password";
+        }
+    }else {
+        $message = "User not found";
+    }
+}
+
+
 include_once 'header.php';
 ?>
 <form action="" method="post">
@@ -14,7 +48,11 @@ include_once 'header.php';
         <label for="password">Password</label>
         <input type="password" name="password" id="password">
     </p>
+
     <button type="submit" name="submit">Login</button>
+    <div>
+        <?php echo $message; ?>
+    </div>
     <p>
         Don't have an account?
         <u>
